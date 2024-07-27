df6af72fa3230fa159b5b941dcc34ccf9330ec3a
Aditya <adityagundg14@gmail.com>
2024-07-27 16:59:11 +0530

    version updated

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom'; // Change to 'react-dom' for React 17
-import App from './App'; // Make sure this path is correct
-import './index.css'; // If you have any global styles
+import ReactDOM from 'react-dom'; // Change this for React 17
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 // Render the App component into the root element
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
